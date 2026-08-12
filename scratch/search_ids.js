b15d0830ed51122f68f25878d85371ce22eb7d8d
Vitor Hugo <vitonhoalmeida@gmail.com>
2026-08-12 10:09:01 -0300

    feat: first commit - structural deploy configurations

--- scratch/search_ids.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('index.html', 'utf8');
-const lines = content.split('\n');
-lines.forEach((l, i) => {
-    if (l.includes('panel-')) {
-        console.log(`${i + 1}: ${l.trim()}`);
-    }
-});
--- scratch/test_acorn_error.js
+++
@@ -1,6 +0,0 @@
-const acorn = require('acorn');
-try {
-    acorn.parse("document.addEventListener('DOMContentLoaded', () => {", { ecmaVersion: 2020 });
-} catch (e) {
-    console.log(e.message);
-}
